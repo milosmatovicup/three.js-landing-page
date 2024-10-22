f4c07e080ffd3aeeaa6cb5e782193e9242051808
procaptain <procaptain1029@gmail.com>
2024-10-22 15:28:21 +0300

    feat(model): add black shoes model

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -1,21 +1,29 @@
 import { Canvas } from "@react-three/fiber";
 import { Environment, OrbitControls } from "@react-three/drei";
-import Shoe from "./models/whiteShoes";
+import WhiteShoe from "./models/whiteShoes";
+import BlackShoe from "./models/blackShoes";
+import Computer from "./models/computer";
 
 const Model = ({scroll, ...props}) => {
     console.log("scroll", scroll);
-    const xxx = () => {
-        console.log("scroll=", scroll);
-    }
     return(
         <Canvas style={{ width: "100%", height: "100vh"}} shadows>
         <color attach="background" args={[0x111111]} />
         <ambientLight intensity={2} />
-        <Shoe />
+        <group position={[0, 5, 0]} scale={[2, 2, 2]}>
+            <WhiteShoe />
+        </group>
+        <group position={[0, 0, 0]}>
+            <BlackShoe />
+        </group>
+        <group position={[0, -5, 0]}>
+            <Computer />
+        </group>
         <Environment preset="city" />
         <OrbitControls 
             enablePan={false}
         />
+        <axesHelper args={[5]} />
       </Canvas>
     )
 }
