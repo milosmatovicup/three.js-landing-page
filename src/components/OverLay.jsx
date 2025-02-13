af00101457c92bfbf69bb0fc061bb09f8aa3d48f
procaptain <procaptain1029@gmail.com>
2025-02-13 18:56:25 +0200

    fix(contact): change contact info

--- src/components/OverLay.jsx
+++ src/components/OverLay.jsx
@@ -22,13 +22,13 @@ const Overlay = forwardRef(({ caption, scroll }, ref) => (
         <div style={{ height: "200vh" }}>
             <div className="dot font-arima-regular max-w-900 leading-30">
                 <h1 className="text-white font-arima-bold">WebGL & Three.js expert</h1>
-                <p className="text-30 mb-10">In today’s fast-paced digital world, websites are no longer just flat, 2D spaces
+                <p className="mb-10 text-30">In today’s fast-paced digital world, websites are no longer just flat, 2D spaces
                     The digital scene is bustling, and to make an impression, websites now need that extra ‘oomph’!
                 </p>
-                <p className="text-30 mb-10">
+                <p className="mb-10 text-30">
                     I have extensive experience in developing landing pages and building construction using Three.js.
                 </p>
-                <p className="text-30 mb-10">
+                <p className="mb-10 text-30">
                     Dive into the world of Three.js with me and make your site the best it can be.
                 </p>
             </div>
@@ -36,20 +36,14 @@ const Overlay = forwardRef(({ caption, scroll }, ref) => (
         <div style={{ height: "200vh" }}>
             <div className="dot font-arima-regular max-w-900 leading-30">
                 <h1 className="text-white font-arima-bold">Contact me</h1>
-                <p className="text-30 mb-10 ml-15">
-                    NickName: Golden Dragon    
+                <p className="mb-10 text-30 ml-15">
+                    NickName: Procaptain 
                 </p>
                 <div>
-                    <a className="pointer-events-auto m-15 flex" target="blank" href="https://github.com/Goldendragon1029">
+                    <a className="flex pointer-events-auto m-15" target="blank" href="https://github.com/Goldendragon1029">
                         <img src="logos/github.svg" alt="github" width={40} height={40}/>
-                        <p className="text-20 m-4 ml-20">
-                            https://github.com/Goldendragon1029
-                        </p>
-                    </a>
-                    <a className="pointer-events-auto m-15 flex" href="mailto:procaptain1029@gmail.com">
-                        <img src="logos/mail.svg" alt="mail" width={40} height={40}/>
-                        <p className="text-20 m-4 ml-20">
-                            procaptain1029@gmail.com
+                        <p className="m-4 ml-20 text-20">
+                            https://github.com/milosmatovicup
                         </p>
                     </a>
                 </div>
